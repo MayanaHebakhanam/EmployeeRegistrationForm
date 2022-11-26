bd5aeba28d254bcc136e1526ab540cfe679f8961
Mayana_Heba_Khanam <mayana.heba@skillassure.com>
2022-11-26 23:32:27 +0530

    file

---
+++ EmployeeRegister/WebContent/delEmp.jsp
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<%@ page import="java.sql.*"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<%  Class.forName("com.mysql.cj.jdbc.Driver"); %>
+	<% int k=0;%>
+	<% String str1=request.getParameter("fname"); %>
+	<%
+try{
+   
+    String sql="delete from employee where firstname='"+str1+"'" ;
+    String url = "jdbc:mysql://localhost:3306/empdb";
+	String username = "root";
+	String pswd = "Mayana_25";
+	Connection connection = DriverManager.getConnection(url, username, pswd);
+	Statement st=connection.createStatement();
+	k=st.executeUpdate(sql);
+}
+catch(Exception e){
+    e.printStackTrace();
+  }
+%>
+	<%if(k>0){%>
+	<%String str2="Successfully removed employee "+str1; %>
+	<% out.println(str2); %>
+	<% } %>
+
+</body>
+</html>
---
+++ EmployeeRegister/src/com/employee/registerForm/EmployeeDAO.java
@@ -0,0 +1,43 @@
+package com.employee.registerForm;
+
+import java.sql.*;
+
+public class EmployeeDAO {
+
+	public int registerEmployee(Employee emp) throws ClassNotFoundException {
+		int result = 0;
+		String emp_sql = "INSERT INTO employee"
+				+ " (firstName, lastName, salary, department, position, email_address, contact_Number) VALUES"
+				+ " (?,?,?,?,?,?,?);";
+
+		String url = "jdbc:mysql://localhost:3306/empdb";
+		String username = "root";
+		String pswd = "Mayana_25";
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		try (Connection con = DriverManager.getConnection(url, username, pswd);
+				PreparedStatement ps = con.prepareStatement(emp_sql)) {
+
+			ps.setString(1, emp.getFirstName());
+			ps.setString(2, emp.getLastName());
+			ps.setLong(3, emp.getSalary());
+			ps.setString(4, emp.getDepartment());
+			ps.setString(5, emp.getPosition());
+			ps.setString(6, emp.getEmail());
+			ps.setString(7, emp.getContactNumber());
+			result = ps.executeUpdate();
+		} catch (SQLException e) {
+			printSQLException(e);
+		}
+		return result;
+	}
+
+	private void printSQLException(SQLException exc) {
+		for (Throwable e : exc) {
+			if (e instanceof SQLException) {
+				e.printStackTrace(System.err);
+			}
+		}
+
+	}
+}
